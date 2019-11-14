a4eb19813e6cc77f286ab908420da08685642449
Philippa Mitchell <philippa_mitchell@hotmail.com>
2019-11-14 15:01:43 +0000

    making progress on doses controller

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient_id
+  validates_uniqueness_of :ingredient, scope: :cocktail_id
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "grapefruit")
+Ingredient.create(name: "pommegranite")
+Ingredient.create(name: "rosemary")
+Ingredient.create(name: "rhubarb gin")
+Ingredient.create(name: "regular gin")
+Ingredient.create(name: "grapefruit gin")
+Ingredient.create(name: "apple juice")
+Ingredient.create(name: "ginger ale")
+Ingredient.create(name: "elderflower tonic")
+
+p Ingredient.all
